dc047ddd1e9cdeef0122328348f4c0aaefb02a1d
Avadhoot Bangal <46864370+elixiruhig@users.noreply.github.com>
2020-03-11 20:04:07 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,6 +2,33 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from fastai.vision import *
+from efficientnet_pytorch import EfficientNet
+
+
+
+
+
+
+
+
+model = EfficientNet.from_name('efficientnet-b6', override_params={'num_classes': 5 })
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=3., reduction='mean'):
+        super().__init__()
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        CE_loss = nn.CrossEntropyLoss(reduction='none')(inputs, targets)
+        pt = torch.exp(-CE_loss)
+        F_loss = ((1 - pt)**self.gamma) * CE_loss
+        if self.reduction == 'sum':
+            return F_loss.sum()
+        elif self.reduction == 'mean':
+            return F_loss.mean()
 
 
 def main():
