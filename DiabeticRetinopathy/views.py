9a21abc2ae97815eaafd145687dfe544da281715
Avadhoot Bangal <46864370+elixiruhig@users.noreply.github.com>
2020-03-12 18:25:00 +0530

    Added the pdf download feature

--- DiabeticRetinopathy/views.py
+++ DiabeticRetinopathy/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from DiabeticRetinopathy.forms import RegisterForm, LoginForm, ReportForm
 from DiabeticRetinopathy.models import Report
+from .render import Render
+
 
 
 
@@ -18,14 +20,32 @@ def homeview(request):
     global load_learner
     if request.user.is_authenticated:
         if request.method =='POST':
+            if 'Download' in request.POST:
+                report = Report.objects.latest('date')
+                params = {
+                    'first_name'    : report.first_name,
+                    'last_name'     : report.last_name,
+                    'age'           : report.age,
+                    'photo'         : report.photo,
+                    'date1'         : report.date,
+                    'category'      : request.session.get('category')
+                }
+                return Render.render('DiabeticRetinopathy/pdf.html', params)
             form = ReportForm(request.POST, request.FILES)
             if form.is_valid():
                 report = form.save(commit=False)
                 report.save()
                 photo = Report.objects.latest('date').photo
+<<<<<<< HEAD
+                learn = load_learner('')
+                category = learn.predict(open_image(photo.path))[0]
+                print(type(category))
+                request.session['category'] = category.__int__()
+=======
 
                 learn = load_learner('')
                 category = learn.predict(open_image(photo.path))[0]
+>>>>>>> 844d5f997221d2ef95e96cdd137b653754f58349
                 return render(request,'DiabeticRetinopathy/report.html',{'photo':photo,'category':category})
         form = ReportForm()
         return render(request, 'DiabeticRetinopathy/homeview.html',{'form':form})
